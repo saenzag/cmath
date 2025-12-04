bb0b346014a0160217716b0c19571a5e2a98f3c3
saenzag <saenzag.imanol@gmail.com>
2025-12-04 10:54:52 +0100

    test removed project config

--- project_config.cmake
+++ project_config.cmake
@@ -1,9 +1,9 @@
 # Project configuration for cmath
-set(USE_SFML OFF)
-set(USE_IMGUI_SFML OFF)
-
-# List of include directories for this project (paths relative to this project folder)
-set(PROJECT_INCLUDE_DIRS
-    "${CMAKE_CURRENT_LIST_DIR}/include"
-    "${CMAKE_CURRENT_LIST_DIR}/vector"
-)
+#set(USE_SFML OFF)
+#set(USE_IMGUI_SFML OFF)
+#
+## List of include directories for this project (paths relative to this project folder)
+#set(PROJECT_INCLUDE_DIRS
+#    "${CMAKE_CURRENT_LIST_DIR}/include"
+#    "${CMAKE_CURRENT_LIST_DIR}/vector"
+#)
