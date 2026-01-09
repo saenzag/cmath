242714c8c2d4c698829e614401ce2c05810c4be7
saenzag <saenzag.imanol@gmail.com>
2026-01-09 13:09:10 +0100

    interface_library

---
+++ matrix/mat4.hpp
@@ -0,0 +1,53 @@
+
+#ifndef __CMATH_MAT4_HPP__
+#define __CMATH_MAT4_HPP__ 1
+
+namespace cmath {
+
+template<typename T>
+struct Mat4
+{
+	static_assert(sizeof(Mat4<T>) == 16 * sizeof(T), "Mat4 size incorrect");
+
+	union
+	{
+		T data[16];
+		struct
+		{
+			T m00, m01, m02, m03;
+			T m10, m11, m12, m13;
+			T m20, m21, m22, m23;
+			T m30, m31, m32, m33;
+		};
+	};
+  
+
+	Mat4() : data{	T(0), T(0), T(0), T(0),
+					T(0), T(0), T(0), T(0),
+					T(0), T(0), T(0), T(0),
+					T(0), T(0), T(0), T(0)}
+    {
+    }
+
+};
+
+template<typename T>
+static Mat4<T> Ortho(T left, T right, T bottom, T top, T nearVal, T farVal)
+{
+	static_assert(left == right || bottom == top || nearVal == farVal, "Invalid ortho parameters");
+
+	Mat4<T> result;
+
+	result.m00 = T(2) / (right - left);
+	result.m11 = T(2) / (top - bottom);
+	result.m22 = T(-2) / (farVal - nearVal);
+	result.m03 = -(right + left) / (right - left);
+	result.m13 = -(top + bottom) / (top - bottom);
+	result.m23 = -(farVal + nearVal) / (farVal - nearVal);
+	result.m33 = T(1);
+
+	return result;
+}
+} // namespace cmath
+
+#endif // __CMATH_MAT4_HPP__
--- project_config.cmake
+++ project_config.cmake
@@ -1,9 +1,12 @@
 # Project configuration for cmath
-#set(USE_SFML OFF)
-#set(USE_IMGUI_SFML OFF)
-#
-## List of include directories for this project (paths relative to this project folder)
-#set(PROJECT_INCLUDE_DIRS
-#    "${CMAKE_CURRENT_LIST_DIR}/include"
-#    "${CMAKE_CURRENT_LIST_DIR}/vector"
-#)
+set(USE_SFML OFF)
+set(USE_IMGUI_SFML OFF)
+
+# List of include directories for this project (paths relative to this project folder)
+set(PROJECT_INCLUDE_DIRS
+    "${CMAKE_CURRENT_LIST_DIR}/include"
+    "${CMAKE_CURRENT_LIST_DIR}/vector"
+    "${CMAKE_CURRENT_LIST_DIR}/matrix"
+)
+
+set(IS_INTERFACE ON)
