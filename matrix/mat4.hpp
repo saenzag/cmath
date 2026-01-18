f19c906a703bc5c3e043434c3da9a61cbddd3ad4
saenzag <saenzag.imanol@gmail.com>
2026-01-18 19:19:39 +0100

    changes

--- matrix/mat4.hpp
+++ matrix/mat4.hpp
@@ -20,16 +20,63 @@ struct Mat4
 	};
   
 
-	Mat4() : data{	
+		Mat4() : data{	
 								T(0), T(0), T(0), T(0),
 								T(0), T(0), T(0), T(0),
 								T(0), T(0), T(0), T(0),
 								T(0), T(0), T(0), T(0)
 							}
-    {
-    }
+			{
+			}
 
+		Mat4(T m00_, T m01_, T m02_, T m03_,
+					T m10_, T m11_, T m12_, T m13_,
+					T m20_, T m21_, T m22_, T m23_,
+					T m30_, T m31_, T m32_, T m33_) :
+			m00(m00_), m01(m01_), m02(m02_), m03(m03_),
+			m10(m10_), m11(m11_), m12(m12_), m13(m13_),
+			m20(m20_), m21(m21_), m22(m22_), m23(m23_),
+			m30(m30_), m31(m31_), m32(m32_), m33(m33_)
+		{
+		}
+
+		~Mat4() = default;
+
+		operator T*() { return data; }
+		operator const T*() const { return data; }
+
+		Mat4<T>& operator*=(const Mat4<T>& other)
+		{
+			*this = *this * other;
+			return *this;
+		}
 };
+
+template<typename T>
+Mat4<T> operator*(const Mat4<T>& mat1, const Mat4<T>& mat2)
+{
+	return Mat4<T> {
+				mat1.m00 * mat2.m00 + mat1.m01 * mat2.m10 + mat1.m02 * mat2.m20 + mat1.m03 * mat2.m30,
+				mat1.m00 * mat2.m01 + mat1.m01 * mat2.m11 + mat1.m02 * mat2.m21 + mat1.m03 * mat2.m31,
+				mat1.m00 * mat2.m02 + mat1.m01 * mat2.m12 + mat1.m02 * mat2.m22 + mat1.m03 * mat2.m32,
+				mat1.m00 * mat2.m03 + mat1.m01 * mat2.m13 + mat1.m02 * mat2.m23 + mat1.m03 * mat2.m33,
+
+				mat1.m10 * mat2.m00 + mat1.m11 * mat2.m10 + mat1.m12 * mat2.m20 + mat1.m13 * mat2.m30,
+				mat1.m10 * mat2.m01 + mat1.m11 * mat2.m11 + mat1.m12 * mat2.m21 + mat1.m13 * mat2.m31,
+				mat1.m10 * mat2.m02 + mat1.m11 * mat2.m12 + mat1.m12 * mat2.m22 + mat1.m13 * mat2.m32,
+				mat1.m10 * mat2.m03 + mat1.m11 * mat2.m13 + mat1.m12 * mat2.m23 + mat1.m13 * mat2.m33,
+
+				mat1.m20 * mat2.m00 + mat1.m21 * mat2.m10 + mat1.m22 * mat2.m20 + mat1.m23 * mat2.m30,
+				mat1.m20 * mat2.m01 + mat1.m21 * mat2.m11 + mat1.m22 * mat2.m21 + mat1.m23 * mat2.m31,
+				mat1.m20 * mat2.m02 + mat1.m21 * mat2.m12 + mat1.m22 * mat2.m22 + mat1.m23 * mat2.m32,
+				mat1.m20 * mat2.m03 + mat1.m21 * mat2.m13 + mat1.m22 * mat2.m23 + mat1.m23 * mat2.m33,
+
+				mat1.m30 * mat2.m00 + mat1.m31 * mat2.m10 + mat1.m32 * mat2.m20 + mat1.m33 * mat2.m30,
+				mat1.m30 * mat2.m01 + mat1.m31 * mat2.m11 + mat1.m32 * mat2.m21 + mat1.m33 * mat2.m31,
+				mat1.m30 * mat2.m02 + mat1.m31 * mat2.m12 + mat1.m32 * mat2.m22 + mat1.m33 * mat2.m32,
+				mat1.m30 * mat2.m03 + mat1.m31 * mat2.m13 + mat1.m32 * mat2.m23 + mat1.m33 * mat2.m33
+	};
+}
 
 template<typename T>
 static Mat4<T> Identity()
@@ -63,8 +110,8 @@ static Mat4<T> Ortho(T left, T right, T bottom, T top, T nearVal, T farVal)
 typedef Mat4<float> Mat4f;
 typedef Mat4<double> Mat4d;
 
-static_assert(sizeof(Mat4f) == 16 * sizeof(float), "Mat4 size incorrect");
-static_assert(sizeof(Mat4d) == 16 * sizeof(double), "Mat4 size incorrect");
+static_assert(sizeof(Mat4f) == 16 * sizeof(float), "Mat4<float> size incorrect");
+static_assert(sizeof(Mat4d) == 16 * sizeof(double), "Mat4<double> size incorrect");
 
 
 } // namespace cmath
--- vector/vec3.hpp
+++ vector/vec3.hpp
@@ -2,6 +2,8 @@
 #ifndef __CMATH_VEC3_H__
 #define __CMATH_VEC3_H__ 1
 
+#include <math.h>
+
 namespace cmath{
   template<typename T>
   struct Vec3
@@ -19,6 +21,121 @@ namespace cmath{
   //     OPERATOR     //
   //////////////////////
 
+    Vec3<T> operator+(const Vec3<T>& other) const
+    {
+      return Vec3<T>(x + other.x,  y + other.y, z + other.z);
+    }
+
+    Vec3<T> operator+=(const Vec3<T>& other)
+    {
+      x += other.x; y += other.y; z += other.z;
+      return *this;
+    }
+
+    Vec3<T> operator+(const T& other) const
+    {
+      return Vec3<T>(x + other,  y + other, z + other);
+    }
+
+    Vec3<T> operator+(T&& other) const
+    {
+      return Vec3<T>(x + other,  y + other, z + other);
+    }
+
+    Vec3<T> operator+=(T&& other)
+    {
+      x += other; y += other; z += other;
+      return *this;
+    }
+
+    Vec3<T> operator+=(const T& other)
+    {
+      x += other; y += other; z += other;
+      return *this;
+    }
+
+    Vec3<T> operator-(const Vec3<T>& other) const
+    {
+      return Vec3<T>(x - other.x,  y - other.y, z - other.z);
+    }
+
+    Vec3<T> operator-=(const Vec3<T>& other)
+    {
+      x -= other.x; y -= other.y; z -= other.z;
+      return *this;
+    }
+
+    Vec3<T> operator-(T&& other) const
+    {
+      return Vec3<T>(x - other,  y - other, z - other);
+    }
+
+    Vec3<T> operator-=(const T& other)
+    {
+      x -= other; y -= other; z -= other;
+      return *this;
+    }
+
+    Vec3<T> operator*(const T scalar) const
+    {
+      return Vec3<T>(x * scalar,  y * scalar, z * scalar);
+    }
+
+    Vec3<T> operator*=(const T scalar)
+    {
+      x *= scalar; y *= scalar; z *= scalar;
+      return *this;
+    }
+
+    Vec3<T> operator/(const T scalar) const
+    {
+      T inv = T(1) / scalar;
+      return *this * inv;
+    }
+
+    Vec3<T> operator/=(const T scalar)
+    {
+      *this *= T(1) / scalar;
+      return *this;
+    }
+
+    // DOT PRODUCT
+    T operator*(const Vec3<T>& other) const
+    {
+      return Dot(*this, other);
+    }
+
+    // CROSS PRODUCT
+    Vec3<T> operator^(const Vec3<T>& other) const
+    {
+      return Cross(*this, other);
+    }
+
+    Vec3<T> operator-() const
+    {
+      return Vec3<T>(-x, -y, -z);
+    }
+
+    Vec3<T> Normalized() const
+    {
+      const T len = Length(*this);
+      if (len > T(0))
+      {
+        return *this / len;
+      }
+      return *this;
+    }
+
+    Vec3<T>& Normalize()
+    {
+      const T len = Length(*this);
+      if (len > T(0))
+      {
+        *this /= len;
+      }
+      return *this;
+    }
+
   //////////////////////
   //       DATA       //
   //////////////////////
@@ -29,6 +146,43 @@ namespace cmath{
     };
 
   }; // !Vector3
+
+  //////////////////////
+  //     OPERATIONS   //
+  //////////////////////
+
+  template<typename T>
+  Vec3<T> Cross(const Vec3<T>& a, const Vec3<T>& b)
+  {
+    return Vec3<T>(
+      a.y * b.z - a.z * b.y,
+      a.z * b.x - a.x * b.z,
+      a.x * b.y - a.y * b.x
+    );
+  }
+
+  template<typename T>
+  T Dot(const Vec3<T>& a, const Vec3<T>& b)
+  {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+  }
+
+  template<typename T>
+  T SquaredLength(const Vec3<T>& v)
+  {
+    return Dot(v, v);
+  }
+
+  template<typename T>
+  T Length(const Vec3<T>& v)
+  {
+    return static_cast<T>(sqrt(Dot(v, v)));
+  }
+  // Specialization for float
+  float Length(const Vec3<float>& v)
+  {
+    return sqrtf(Dot(v, v));
+  }
 
     // Types
   typedef Vec3<int> Vec3i;
